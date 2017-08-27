d9aeb4808068240cc40618de4c262334fb06a82e
root <root@localhost.localdomain>
2017-08-27 19:34:42 +0200

    v2.0

--- src/test.ino
+++ src/test.ino
@@ -15,7 +15,7 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(1000);
+  delay(2000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
