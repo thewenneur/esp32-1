e96594f6c2ec93e02a1cd5e6083ef3893a411de3
root <root@localhost.localdomain>
2017-08-28 18:33:28 +0200

    test nouvelle installation.

--- src/test.ino
+++ src/test.ino
@@ -15,7 +15,7 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(2000);
+  delay(1000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
